33e17dfccee28e23d65938a6358b9e5c3519de68
Chris Mika <chrismika93@gmail.com>
2017-09-15 14:08:51 -0500

    changes install-gcloud.hs to use default gcloud

--- install-gcloud.sh
+++ install-gcloud.sh
@@ -1,4 +1,5 @@
-export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
+export GCLOUD_PATH=/opt/google-cloud-sdk/bin/gcloud
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 ${GCLOUD_PATH} version
 ${GCLOUD_PATH} components update
 ${GCLOUD_PATH} components update kubectl
