6f304635f104346995cfe58a7ce8640f1146f5bc
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 16:21:02 -0500

    Testing build config.yml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 ${GCLOUD_PATH} docker -- push gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1
-~/google-cloud-sdk/bin/kubectl run hello-node --image=gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1 --port=8080
+~/google-cloud-sdk/bin/kubectl run hello-node --image=gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1 --port=80
 ~/google-cloud-sdk/bin/kubectl expose deployment hello-node --type=LoadBalancer
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
