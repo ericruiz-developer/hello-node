0fc550bc5a48f4319b6c16f3577c57e7a3089841
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 13:48:06 -0500

    Testing build config.yml

--- auth.sh
+++ auth.sh
@@ -1,9 +1,9 @@
 export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 echo ${GOOGLE_AUTH} | base64 --decode -i > ${HOME}/gcp-key.json
-${GCLOUD_PATH} --quiet auth activate-service-account --key-file=${HOME}/gcp-key.json
-${GCLOUD_PATH} --quiet config set project ${GOOGLE_PROJECT_ID}
-${GCLOUD_PATH} --quiet config set container/cluster ${GOOGLE_CLUSTER_NAME}
-${GCLOUD_PATH} --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-${GCLOUD_PATH} --quiet config set container/use_application_default_credentials true
-${GCLOUD_PATH} --quiet config set container/use_client_certificate true
+${GCLOUD_PATH} auth activate-service-account --key-file=${HOME}/gcp-key.json
+${GCLOUD_PATH} config set project ${GOOGLE_PROJECT_ID}
+${GCLOUD_PATH} config set container/cluster ${GOOGLE_CLUSTER_NAME}
+${GCLOUD_PATH} config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+${GCLOUD_PATH} config set container/use_application_default_credentials true
+${GCLOUD_PATH} config set container/use_client_certificate true
 ${GCLOUD_PATH} container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
--- install-gcloud.sh
+++ install-gcloud.sh
@@ -2,5 +2,5 @@ export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 curl https://sdk.cloud.google.com | bash
 ${GCLOUD_PATH} version
-${GCLOUD_PATH} --quiet components update
-${GCLOUD_PATH} --quiet components update kubectl
+${GCLOUD_PATH} components update
+${GCLOUD_PATH} components update kubectl
