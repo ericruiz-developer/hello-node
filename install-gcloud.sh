306f278798ba9e717f7b189af9fbb3ae0a675c05
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 12:54:08 -0500

    Testing build config.yml

---
+++ install-gcloud.sh
@@ -0,0 +1,6 @@
+export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+curl https://sdk.cloud.google.com | bash
+${GCLOUD_PATH} version
+${GCLOUD_PATH} --quiet components update
+${GCLOUD_PATH} --quiet components update kubectl
