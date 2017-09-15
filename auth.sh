2e232284f57763cf812cf154f8bf363c9c0095ed
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 13:54:45 -0500

    Testing build config.yml

--- auth.sh
+++ auth.sh
@@ -1,6 +1,6 @@
 export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 echo ${GOOGLE_AUTH} | base64 --decode -i > ${HOME}/gcp-key.json
-echo ${HOME}/gcp-key.json
+cat ${HOME}/gcp-key.json
 #${GCLOUD_PATH} auth activate-service-account --key-file=${HOME}/gcp-key.json
 #${GCLOUD_PATH} config set project ${GOOGLE_PROJECT_ID}
 #${GCLOUD_PATH} config set container/cluster ${GOOGLE_CLUSTER_NAME}
