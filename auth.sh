06e0065b8d8dd8dcedc6e571b4889bf5d9152ebe
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 13:58:54 -0500

    Testing build config.yml

--- auth.sh
+++ auth.sh
@@ -1,10 +1,9 @@
 export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 echo ${GOOGLE_AUTH} | base64 --decode -i > ${HOME}/gcp-key.json
-cat ${HOME}/gcp-key.json
-#${GCLOUD_PATH} auth activate-service-account --key-file=${HOME}/gcp-key.json
-#${GCLOUD_PATH} config set project ${GOOGLE_PROJECT_ID}
-#${GCLOUD_PATH} config set container/cluster ${GOOGLE_CLUSTER_NAME}
-#${GCLOUD_PATH} config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-#${GCLOUD_PATH} config set container/use_application_default_credentials true
-#${GCLOUD_PATH} config set container/use_client_certificate true
-#${GCLOUD_PATH} container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
+${GCLOUD_PATH} auth activate-service-account --key-file=${HOME}/gcp-key.json
+${GCLOUD_PATH} config set project ${GOOGLE_PROJECT_ID}
+${GCLOUD_PATH} config set container/cluster ${GOOGLE_CLUSTER_NAME}
+${GCLOUD_PATH} config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+${GCLOUD_PATH} config set container/use_application_default_credentials true
+${GCLOUD_PATH} config set container/use_client_certificate true
+${GCLOUD_PATH} container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
