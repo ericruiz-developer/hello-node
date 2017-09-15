107eee0bb8fb9188780f70f60985879764e009d0
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 14:32:28 -0500

    Testing build config.yml

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 ${GCLOUD_PATH} docker -- push gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1
--- install-gcloud.sh
+++ install-gcloud.sh
@@ -1,4 +1,3 @@
-export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 curl https://sdk.cloud.google.com | bash
 ${GCLOUD_PATH} version
