581725833b29f0baac61c5b5fb5fed3535ecd137
ericruiz-developer <ruiz.eric@heb.com>
2017-09-15 15:54:31 -0500

    Testing build config.yml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export GCLOUD_PATH=~/google-cloud-sdk/bin/gcloud
 ${GCLOUD_PATH} docker -- push gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1
-kubectl run hello-node --image=gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1 --port=8080
-kubectl expose deployment hello-node --type=LoadBalancer
+~/google-cloud-sdk/bin/kubectl run hello-node --image=gcr.io/${GOOGLE_PROJECT_ID}/hello-node:v1 --port=8080
+~/google-cloud-sdk/bin/kubectl expose deployment hello-node --type=LoadBalancer
